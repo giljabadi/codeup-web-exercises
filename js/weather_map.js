cb9c4504f56c4961500c40e0ff5cce8cf8ccbc69
Gil Jacob Abadi <giljabadi@gmail.com>
2022-11-28 09:43:19 -0600

    completed api key pull

---
+++ js/weather_map.js
@@ -0,0 +1,12 @@
+//
+//
+// $.get("https://api.openweathermap.org/data/3.0/onecall", {
+//     APPID: OPEN_WEATHER_APPID,
+//     lat:    32.7692532,
+//     lon:   -96.7972702,
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log('The entire response:', data);
+//     console.log('Diving in - here is current information: ', data.current);
+//     console.log('A step further - information for tomorrow: ', data.daily[1]);
+// });
